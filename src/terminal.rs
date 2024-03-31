69a0194fc85ca36ef0ce5a3740605baa80bfe055
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-03-31 14:56:07 +0200

    fixes no key release bug

--- src/terminal.rs
+++ src/terminal.rs
@@ -4,10 +4,15 @@ pub struct Terminal(pub ratatui::Terminal<ratatui::backend::CrosstermBackend<std
 
 impl std::ops::Drop for Terminal {
     fn drop(&mut self) {
-        crossterm::terminal::disable_raw_mode().expect("leave terminal raw mode");
+        crossterm::execute!(
+            std::io::stdout(),
+            crossterm::event::PopKeyboardEnhancementFlags
+        )
+        .expect("pop keyboard extentions");
         std::io::stdout()
             .execute(crossterm::terminal::LeaveAlternateScreen)
             .expect("leave terminal alternate screen");
+        crossterm::terminal::disable_raw_mode().expect("leave terminal raw mode");
     }
 }
 
@@ -17,6 +22,15 @@ impl Terminal {
     pub fn new() -> Result<Self, std::boxed::Box<dyn std::error::Error>> {
         crossterm::terminal::enable_raw_mode()?;
         std::io::stdout().execute(crossterm::terminal::EnterAlternateScreen)?;
+        crossterm::execute!(
+            std::io::stdout(),
+            crossterm::event::PushKeyboardEnhancementFlags(
+                crossterm::event::KeyboardEnhancementFlags::REPORT_EVENT_TYPES
+                    | crossterm::event::KeyboardEnhancementFlags::REPORT_ALTERNATE_KEYS
+                    | crossterm::event::KeyboardEnhancementFlags::REPORT_ALL_KEYS_AS_ESCAPE_CODES
+                    | crossterm::event::KeyboardEnhancementFlags::DISAMBIGUATE_ESCAPE_CODES
+            )
+        )?;
 
         let backend = ratatui::backend::CrosstermBackend::new(std::io::stdout());
         Ok(Self(ratatui::Terminal::new(backend)?))
